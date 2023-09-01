7f95972f6344cb4f0a0ea9052588a80613ef44a9
Michael Hannah <Michaelrkhannah@gmail.com>
2023-09-01 07:05:50 -0600

    Moved some stuff around and edited all data in JS

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,92 @@
-// TODO: Include packages needed for this application
+// Required rackages for application
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const fs = require("fs");
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const path = require("path");
 
-// TODO: Create a function to initialize app
-function init() {}
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// Function call to initialize app
+// This is an array of question for prompts
+const questions = [
+
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
+
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?"
+    },
+
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a description of your project:"
+    },
+
+    {
+        type: "input",
+        name: "github",
+        message: "What is you GitHub username?"
+    },
+
+    {
+        type: "list",
+        name: "license",
+        message: "What license does our project have?",
+        choices: ["MIT", "APACHE2.0", "Boost1.0", "GLP3.0", "BSD2", "BSD3", "None"]
+    },
+
+    {
+        type: "input",
+        name: "dependencies",
+        message: "Any dependencies to install?",
+        default: "npm i"
+    },
+
+    {
+        type: "input",
+        name: "test",
+        message: "What command should be run to run tests?",
+        default: "npm test"
+    },
+
+    {
+        type: "input",
+        name: "usage",
+        message: "What is the proper usage of this repo?"
+    },
+
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who are any contributors of this repo?"
+    }
+];
+
+// this is the function to wirte file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+// This will start up the app
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerAnswers) => {
+        console.log("Generating.... Please wait....");
+        writeToFile("./utils/sampleREADME.md", generateMarkdown({ ...inquirerAnswers }));
+    })
+}
+
+// Call of the function that starts app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,89 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Returns an empty string if no license
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![Github License](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+  }
+  return ""
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
 
-// TODO: Create a function to generate markdown for README
+// Empty string return if no license
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return (
+      `* [License](#license)`
+    )
+  }
+  return ""
+}
+
+// Empty string return if no license
+function renderLicenseSect(license) {
+  if (license !== "None") {
+    return (
+   `## License  
+
+    Copyright @ ${license}. All rights reserved.
+
+    Licensed under the ${license} license.`
+    )
+  }
+  return ""
+}
+
+// This function will generate markdown Doc
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
-`;
+
+  ## Description 
+
+  ${data.description}
+
+  ## Table of Contents 🗒
+
+  * [Installations](#installations)
+
+  * [Usage](#usage)
+  
+  * [Contributors](#contributors)
+
+  * [Test](#tests)
+
+  * [Questions](#questions)
+
+  ${renderLicenseLink(data.license)}
+  
+  ## Installations 
+
+  To install dependencies, run these commands:
+
+  ${data.dependencies}
+  
+  ## Usage
+
+  ${data.usage}
+
+  ${renderLicenseSect(data.license)}
+
+  ## Contributors
+
+  ${data.contributors}
+
+  ## Tests
+
+  To run tests, run these commands:
+
+  ${data.test}
+
+  ## Questions
+
+  For additional questions, contact me at the email provided below. 
+
+  - GitHub: [${data.github}](https://github.com/${data.github}/)
+  - Email:  ${data.email}`;
 }
 
 module.exports = generateMarkdown;
